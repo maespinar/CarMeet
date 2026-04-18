e46a9fd3827ee829bbd01eaab278078957d4cdfc
Espinar <matiasespinar06@gmail.com>
2026-04-17 23:34:31 -0500

    commit arreglosFinalesEnEvaluacionVehiculos

--- src/Acciones/EvaluacionVehiculos.java
+++ src/Acciones/EvaluacionVehiculos.java
@@ -3,25 +3,32 @@ package Acciones;
 
 import Interfaces.Auditable;
 import Personas.Expositor;
+import Personas.Staff;
 import Vehiculos.Vehiculo;
 
 public class EvaluacionVehiculos {
-    for (int i = 0; i < expositores.length(); i++) {
-            Expositor expositorActual = listaExpositores.get(i);
+    
+    public EvaluacionVehiculos() {    
+    }
+    
+    public void auditarLista(Expositor[] gestorExpositores, Staff asesorEncargado) {
+        for (int i = 0; i < gestorExpositores.length; i++) {
+            Expositor expositorActual = gestorExpositores[i];
             Vehiculo autoActual = expositorActual.getCarro();
             if (autoActual instanceof Auditable) {
                 Auditable autoAuditable = (Auditable) autoActual;
                 
                 if (autoAuditable.cumpleRequisitos()) {
                     System.out.println("\n" + "Felicidades! Vehiculo aceptado.");
-                    asesor1.aprobarPropietario();
+                    asesorEncargado.aprobarPropietario();
                     expositorActual.mostrarDetalles();
                     System.out.println("Su puntaje de exhibicion es: " + expositorActual.getCarro().calcularPuntajeExhibicion(expositorActual.getCarro()) + "\n");
                 } else {
                     System.out.println("\n" + "El vehiculo de placa " + expositorActual.getCarro().getPlaca() + " no cumple con los requisitos");
                     System.out.println("El expositor " + expositorActual.getNombre() + " ha sido descalificado");
                 }
-            }
-            
+            }   
+        }
     }
+    
 }
